d07cb8f1dcba257e20d13c758f3a0e53ca270835
Teeeep <casperjantepe@gmail.com>
2019-02-15 11:20:15 +0100

    make data array

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const cardList  = [
+    card1 = { 
+        question: 'What does DOM stand for?',
+        answer: 'Document Object Model'
+    },
+    card2 = {
+        question: 'What are primitive data types?',
+        answer: 'Number, string, Boolean'
+    },
+    card3 = {
+        question: `How can you acces an Element using it's ID using JS?`,
+        answer: 'call getElementById on the document'
+    },
+    card4 = {
+        question: 'What method do you use to create an element?',
+        answer: 'createElement'
+    }]
